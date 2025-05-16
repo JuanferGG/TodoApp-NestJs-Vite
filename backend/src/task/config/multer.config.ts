74a28768ae0b6711f0cc23e62c1146b5e2763864
JuanferGG <juanfe15777@gmail.com>
2025-05-16 14:51:31 -0500

    fix multer.config.ts crear carpeta si no existe

--- backend/src/task/config/multer.config.ts
+++ backend/src/task/config/multer.config.ts
@@ -1,11 +1,17 @@
 import { BadRequestException } from '@nestjs/common';
 import { diskStorage } from 'multer';
 import { extname, join } from 'path';
+import * as fs from 'fs';
 
 export const multerConfig = {
   storage: diskStorage({
     destination: (req, file, cb) => {
-      cb(null, join(process.cwd(), 'uploads', 'tasks'));
+      const uploadPath = join(process.cwd(), 'uploads', 'tasks');
+      // TODO: Crear el directorio si no existe
+      if (!fs.existsSync(uploadPath)) {
+        fs.mkdirSync(uploadPath, { recursive: true });
+      }
+      cb(null, uploadPath);
     },
     filename: (req, file, cb) => {
       const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
